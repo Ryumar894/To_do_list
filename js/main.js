a0de9f5dc76b81662f6f633ce54fc1f4e3af001f
Omar Marquez <wuatifor@hotmail.com>
2025-04-21 23:40:46 -0600

    Mejora de la apariencia, añadi boton de checkbox y para eliminar algo del listado asi como su responsive

--- js/main.js
+++ js/main.js
@@ -1,37 +1,91 @@
-function getValue() { // Funcion para sacar el valor de un input al presionar un boton
+/*function getValue() { // Función para obtener el valor del input y agregarlo a la lista
+  const tareaNueva = document.getElementById("add_task"); // Guardamos el input en una variable
+  const valor = tareaNueva.value; // Obtenemos el valor del input
 
-	let tareaNueva = document.getElementById("add_task"); // Guardamos el input en una variable
- 
-	let valor = tareaNueva.value; // De la variable del input, sacamos el value
-  
-  if (valor.length == 0 || /^\s+$/.test(valor)) { // Validar si el input esta vacío
-  
-  	alert("Añada una tarea a la lista"); // Mostrar un alert y no añadir nada en blanco
-    
+  // Validamos si el input está vacío o solo tiene espacios
+  if (valor.length == 0 || /^\s+$/.test(valor)) {
+    alert("Añada una tarea a la lista"); // Si está vacío, mostramos un mensaje
   } else {
-   		const list = document.createElement("li"); // Crea un nuevo li
-  
-  		let listNew = document.createTextNode(valor); // Tomamos el value del input y creamos un nodo de texto
-  
-  		list.appendChild(listNew); // Agregamos el nodo al elemento p
-  
-  		const element = document.getElementById("ul_list"); // Ahora seleccionamos el UL en donde irá
-  
-  		element.appendChild(list); // Hacemos un appendChild del nuevo li con al ul_list
+    const list = document.createElement("li"); // Creamos un nuevo <li> para la tarea
+
+    // Creamos el checkbox para marcar la tarea
+    const chkbox = document.createElement("input");
+    chkbox.setAttribute("type", "checkbox"); // Establecemos el tipo de input como checkbox
+    chkbox.classList.add("check"); // Le asignamos una clase para el estilo
+
+    // Creamos el texto de la tarea dentro de un <span>
+    const span = document.createElement("span");
+    span.setAttribute("class", "task-text"); // Le asignamos una clase para poder estilizarlo
+    span.textContent = valor; // Asignamos el valor de la tarea como texto del span
+
+    // Creamos la imagen de eliminar tarea
+    const image = document.createElement("img");
+    image.setAttribute("src", "./assets/img/trash.svg"); // Ruta de la imagen de la papelera
+    image.classList.add("trash"); // Le asignamos una clase para el estilo
+    image.setAttribute("onclick", "remove(this.parentElement)"); // Cuando se haga clic, se eliminará el li
+
+    // Agregamos los elementos al <li> (checkbox, texto y la imagen de eliminar)
+    list.appendChild(chkbox); // Agregamos el checkbox al li
+    list.appendChild(span); // Agregamos el texto al li
+    list.appendChild(image); // Agregamos la papelera al li
+
+    // Añadimos el <li> a la lista (ul) en el HTML
+    document.getElementById("ul_list").appendChild(list);
   }
-  
-  
+}*/
+function getValue() {
+  const tareaNueva = document.getElementById("add_task");
+  const valor = tareaNueva.value;
+
+  if (valor.length === 0 || /^\s+$/.test(valor)) {
+      alert("Añada una tarea a la lista");
+  } else {
+      const taskElement = createTaskElement(valor);
+      document.getElementById("ul_list").appendChild(taskElement);
+  }
+
+  clean();
 }
+
+function createTaskElement(valor) {
+  const list = document.createElement("li");
+  const chkbox = document.createElement("input");
+  chkbox.classList.add("check");
+  chkbox.type = "checkbox";
+
+  const span = document.createElement("span");
+  span.classList.add("task-text");
+  span.textContent = valor;
+
+  const image = document.createElement("img");
+  image.classList.add("trash");
+  image.src = "./assets/img/trash.svg";
+  image.onclick = function() {
+      remove(list); // Pasamos el 'li' a la función remove
+  };
+
+  list.appendChild(chkbox);
+  list.appendChild(span);
+  list.appendChild(image);
+
+  return list;
+}
+
 
 function clean() {
-  document.getElementById('add_task').value = ''; // Toma al value del input y borra el contenido
+  document.getElementById('add_task').value = ''; // Limpiamos el valor del input después de agregar la tarea
 }
 
-const input = document.getElementById("add_task"); // Llamo al input para identificar cuando se presione el Enter
-
-input.addEventListener("keypress", (event) => { // Verificamos si se presiono la tecla, validamos y mandamos a llamar las funciones que realiza el boton
-    if (event.key === "Enter") { // Si el evento de la tecla es igual a Enter, imprimimos y limpiamos el input
-        getValue();
-        clean();
-    }
+const input = document.getElementById("add_task"); // Seleccionamos el input
+input.addEventListener("keypress", (event) => { // Añadimos un evento para escuchar cuando se presiona "Enter"
+  if (event.key === "Enter") { // Si la tecla presionada es "Enter", ejecutamos getValue y limpiamos el input
+    getValue();
+  }
 });
+
+function remove(el) { // Función para eliminar un <li> al hacer clic en la imagen de la papelera
+  el.classList.add("fade-out"); // Añadimos una clase de animación para desvanecer el li
+  setTimeout(() => { // Esperamos a que termine la animación y luego eliminamos el <li>
+    el.remove();
+  }, 500); // Tiempo de espera antes de eliminar (500ms para que se complete la animación)
+}
